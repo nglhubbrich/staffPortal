2a2a1bdab3318e5ca67e28fe4ef36f2c18f9fb90
Staff Portal <nglhubbardrichards@icloud.com>
2018-11-22 14:13:16 -0600

    Set up roles and redirect on login

--- renderer.js
+++ renderer.js
@@ -9,15 +9,34 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var db = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+db.settings(settings);
+
 var loginBtn = document.getElementById('loginBtn');
 
 loginBtn.addEventListener('click', function() {
     var userName = document.getElementById('usernameField').value;
     var password = document.getElementById('passwordField').value;
     firebase.auth().signInWithEmailAndPassword(userName, password).then(function(){
-        window.location.assign('profilePage.html');
+        var userID = firebase.auth().currentUser.uid;
+        var docRef = db.collection('roles').doc(userID);
+
+        docRef.get().then(function (doc) {
+            if (doc && doc.exists) {
+                const roleData = doc.data();
+                var userRole = roleData.role;
+            } if (userRole === 'super') {
+                window.location.assign('superProfilePage.html');
+            } else {
+                window.location.assign('staffProfilePage.html');
+            }
+        }).catch(function (error) {
+            console.log('Got an error: ', error);
+        });
     }).catch(function(error){
-        console.log(error.message);
+        alert(error.message);
+        console.log('Oops! Something went wrong. Try that again.');
         return;
     })
 });
